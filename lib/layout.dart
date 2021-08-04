82422b547fa17a32f01a08ce18d91ddda0c5a59b
nayyara-airlangga <nayyara.airlangga@gmail.com>
2021-08-05 01:02:39 +0700

    Created top navbar method

--- lib/layout.dart
+++ lib/layout.dart
@@ -2,19 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'widgets/large_screen.dart';
 import 'widgets/small_screen.dart';
+import 'widgets/top_nav.dart';
 import 'helpers/responsive_widget.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({
+  SiteLayout({
     Key key,
   }) : super(key: key);
+
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
+      appBar: topNavigationBar(
+        context: context,
+        key: scaffoldKey,
       ),
       body: ResponsiveWidget(
         largeScreen: LargeScreen(),
